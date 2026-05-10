2e6d06d3723f8610f931483c26db4609ad064732
Huỳnh Nguyễn Minh Như <s4104540@rmit.edu.vn>
2026-05-10 17:05:29 +0700

    Add files via upload

--- helper.py
+++ helper.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
 def data_source(file_name):
-    df=pd.read_csv('don_hang.csv',index_col=False)
-    df['ngay_dat']= pd.to_datetime(df['ngay_dat'],format ='mixed')
-    df['thanh_tien']=df['so_luong']* df['don_gia']
+    df = pd.read_csv('don_hang.csv', index_col=False)
+    # Drop any leftover index columns saved by previous to_csv() calls
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    df['ngay_dat'] = pd.to_datetime(df['ngay_dat'], format='mixed')
+    df['thanh_tien'] = df['so_luong'] * df['don_gia']
     return df
 
